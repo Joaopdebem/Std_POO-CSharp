e9927a3ce4128d4180d3b41923f9372a6b44f527
João Pedro de Bem <jpdebem@gmail.com>
2023-09-20 11:37:58 -0300

    Subindo arquivos

---
+++ Payments/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Payments
+{
+	// Tipos genéricos
+	class Program {
+		static void Main(string[] args)
+		{
+			IEnumerable<Payment> paymentsEnum = new List<Payment>();
+
+			ICollection<string> lista = new List<string>();
+
+			IList<Payment> payments = new List<Payment>();
+			payments.Add(new Payment(2));
+			payments.Add(new Payment(1));
+			payments.Add(new Payment(3));
+
+			foreach( var item in payments){
+				Console.WriteLine(item.Id);
+			}
+
+			var paidPayments = new List<Payment>();
+			paidPayments.AddRange(payments);
+
+			var payment = payments.Where(x => x.Id == 3);
+
+
+		}
+	}
+
+	public class Payment 
+	{
+		public int Id { get; set; }
+		public Payment(int id)
+		{
+			Id = id;
+		}
+	}
+
+}
